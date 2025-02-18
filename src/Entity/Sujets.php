e67512a02aa14a0c9af4f728ed608abe52f1d9ff
Abigail <abigail.germon@etu.unilim.fr>
2025-02-18 19:42:45 +0100

    ciel

---
+++ src/Entity/Sujets.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SujetsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SujetsRepository::class)]
+class Sujets
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
